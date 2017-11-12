a766b60c6c4d1a8f30b5b1b2a161d98952cad8d2
Eloydrummerboy <eloydrummerboy@gmail.com>
2017-11-11 23:49:19 -0500

    Initial upload

---
+++ Into_To_Machine_Learning/Lesson 3 - SVM/ClassifySVM.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 11 15:05:05 2017
+
+@author: Scott
+"""
+
+def classify(features_train, labels_train):   
+    ### import the sklearn module for GaussianNB
+    ### create classifier
+    ### fit the classifier on the training features and labels
+    ### return the fit classifier
+    
+    
+    ### your code goes here!
+    
+    from sklearn.svm import SVC
+    clf = SVC(kernel='linear')
+    return clf.fit(features_train, labels_train)
+    
